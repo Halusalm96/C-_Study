e511d6c546242ef7b53159796d9ce87709dc9c6d
Halusalm96 <dmadkr6004@naver.com>
2024-11-21 17:41:42 +0900

    초기 커밋.

---
+++ 001_Mission/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Mission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1번째 방법
+            Console.WriteLine("  *  ");
+            Console.WriteLine(" *** ");
+            Console.WriteLine("*****");
+            Console.WriteLine(" *** ");
+            Console.WriteLine("  *  ");
+
+            // 2번째 방법
+            Console.Write("   ");
+            Console.WriteLine("*");
+            Console.Write("  ");
+            Console.WriteLine("***");
+            Console.Write(" ");
+            Console.WriteLine("*****");
+            Console.Write("  ");
+            Console.WriteLine("***");
+            Console.Write("   ");
+            Console.WriteLine("*");
+
+        }
+    }
+}
---
+++ 002_Variable/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 변수 선언의 기초 방법
+        }
+    }
+}
