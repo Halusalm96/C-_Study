f4f609949ce8d60bfd5ce75c9d1cd157acd5738e
Halusalm96 <dmadkr6004@naver.com>
2024-11-22 16:55:43 +0900

    test

--- 002_Variable/Program.cs
+++ 002_Variable/Program.cs
@@ -11,6 +11,22 @@ namespace _002_Variable
         static void Main(string[] args)
         {
             // 변수 선언의 기초 방법
+            {
+                int number;
+                number = 10;
+                Console.WriteLine("number : " + number);
+            }
+
+            // 변수 선언의 다양한 방법
+            {
+                int x;
+                int a, b, c;
+
+                int xx = 100;
+                int aa = 0, bb = 1, cc = 2;
+
+                
+            }
         }
     }
 }
