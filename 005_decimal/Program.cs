1b5d91e3b70e80f9c84b93945696dd874199168a
Halusalm96 <dmadkr6004@naver.com>
2024-11-25 16:18:19 +0900

    11-25-decimal

---
+++ 005_decimal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_decimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                decimal dcNum = 123123123.12314M; //수치 접미사 (M, m)
+                Console.WriteLine($"dcNum : {dcNum}");
+            }
+
+            // 타입 & 크기
+            {
+                Console.WriteLine("\n 타입 & 크기");
+
+                decimal dcNum = 123123123.12314M; //수치 접미사 (M, m)
+                Console.WriteLine($"Type: {dcNum.GetType()} sizeof: {sizeof(decimal)} byte");
+            }
+
+            // 저장 가능 범위
+            {
+                Console.WriteLine("\n 저장 가능 범위");
+
+                decimal dcNum = 123123123.12314M; //수치 접미사 (M, m)
+                decimal minDecimal = decimal.MinValue;
+                decimal maxDecimal = decimal.MaxValue;
+
+                Console.WriteLine($"minDecimal: {minDecimal}");
+                Console.WriteLine($"maxDecimal: {maxDecimal}");
+            }
+        }
+    }
+}
