0aaf1e1eaf9ad5cc28a725f1d4116033cc8045cd
Halusalm96 <dmadkr6004@naver.com>
2024-11-25 15:53:23 +0900

    11-25-Float, Double

---
+++ 002_Mission/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Mission
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte minSbyte = sbyte.MinValue;
+            sbyte maxSbyte = sbyte.MaxValue;
+
+            byte minByte = byte.MinValue;
+            byte maxByte = byte.MaxValue;
+
+            short minShort = short.MinValue;
+            short maxShort = short.MaxValue;
+
+            ushort minUShort = ushort.MinValue;
+            ushort maxUShort = ushort.MaxValue;
+
+            int minInt = int.MinValue;
+            int maxInt = int.MaxValue;
+
+            long minLong = long.MinValue;
+            long maxLong = long.MaxValue;
+
+            ulong minULong = ulong.MinValue;
+            ulong maxULong = ulong.MaxValue;
+
+            Console.WriteLine($"sbyte \t Min : {minSbyte} \t ~ Max : {maxSbyte}");
+            Console.WriteLine($"byte \t Min : {minByte} \t ~ Max : {maxByte}");
+            Console.WriteLine($"short \t Min : {minShort} \t ~ Max : {maxShort}");
+            Console.WriteLine($"ushort \t Min : {minUShort}  \t ~ Max : {maxUShort}");
+            Console.WriteLine($"int \t Min : {minInt} \t ~ Max : {maxInt}");
+            Console.WriteLine($"long \t Min : {minLong} \t ~ Max : {maxLong}");
+            Console.WriteLine($"ulong \t Min : {minULong} \t ~ Max : {maxULong}");
+
+        }
+    }
+}
---
+++ 004_Float_Double/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Float_Double
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                float fNum = 0.001f;
+                double dNum = 12345678.9;
+
+                Console.WriteLine($"fNum : {fNum}");
+                Console.WriteLine($"dNum : {dNum}");
+            }
+
+            // 수치 접미사
+            {
+                Console.WriteLine("\n수치 접미사");
+
+                float fNum = 0.00014F; // float (F, f)
+                double dNum = 12345678.9d; // double (D, d)
+
+                Console.WriteLine($"fNum : {fNum}");
+                Console.WriteLine($"dNum : {dNum}");
+            }
+
+            // 타입 & 크기
+            { 
+                Console.WriteLine("\n타입 & 크기");
+
+                float fNum = 0.0032f;
+
+                Console.WriteLine($"Type : {fNum.GetType()} sizeof : {sizeof(float)} byte");
+                Console.WriteLine($"Type : {1412451.14234.GetType()} sizeof : {sizeof(double)} byte");
+            }
+
+            // 저장 가능 범위
+            {
+                Console.WriteLine("\n저장 가능 범위");
+                
+                float minFloat = float.MinValue;
+                float maxFloat = float.MaxValue;
+
+                Console.WriteLine($"minFloat : {minFloat}");
+                Console.WriteLine($"maxFloat : {maxFloat}");
+            }
+
+            // 지수 표현 방식
+            {
+                Console.WriteLine("\n지수 표현 방식");
+
+                float fNum = 1.2345f;
+                Console.WriteLine($"fNum: {fNum}");
+
+                fNum = 12.345e-1f;//12.345 X 10(-1)
+                Console.WriteLine($"fNum: {fNum}");
+
+                fNum = 123.45e-2f;//123.45 X 10(-2)
+                Console.WriteLine($"fNum: {fNum}");
+
+                fNum = 0.12345e1f;//0.12345 X 10(1)
+                Console.WriteLine($"fNum: {fNum}");
+
+                fNum = 0.012345e2f;//0.012345 X 10(2)
+                Console.WriteLine($"fNum: {fNum}");
+
+                double dNum = 1e8;  //1.0 * 10(8)
+                Console.WriteLine($"dNum: {dNum}");
+
+                dNum = 0.1e9;  //0.1 * 10(9)
+                Console.WriteLine($"dNum: {dNum}");
+
+                //부동 소수점에 연산 오류
+                fNum = 100000000000f;
+                for (long i = 0; i < 100000000; i++)
+                    fNum = fNum + 1f;
+                Console.WriteLine($"fNum: {fNum}");
+            }
+        }
+    }
+}
