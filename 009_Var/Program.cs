834b985f89f4ab06d4f4cd9e7f705faec0d65882
Halusalm96 <dmadkr6004@naver.com>
2024-11-26 10:27:09 +0900

    11-26-var, nullable

---
+++ 009_Var/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _009_Var
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // var - 지역 변수로만 사용 가능 / 클래스 안에서, 함수의 반환형, 함수의 파라미터, null 초기값 으로 사용 불가
+
+            var numVar = 100;
+            var numFVar = 0.00314f;
+
+            Console.WriteLine($"numVar : {numVar}");
+            Console.WriteLine($"numFVar : {numFVar}");
+        }
+    }
+}
---
+++ 010_Nullable/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _010_Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nullable - null이 사용 가능하게 만든다.
+            //int? num = null;
+            //double? dNum = null;
+
+            int? num = 100;
+            double? dNum = 3112312.1445d;
+
+            Console.WriteLine($"num : {num}");
+            Console.WriteLine($"num : {num.Value}");
+            Console.WriteLine($"num : {num.HasValue}");
+            Console.WriteLine($"num : {num.GetType()}");
+
+            // 디폴트?? 값을 가져온다??
+            Console.WriteLine($"num : {num.GetValueOrDefault()}");
+
+            Console.WriteLine($"dNum : {dNum}");
+            Console.WriteLine($"dNum : {dNum.Value}");
+            Console.WriteLine($"dNum : {dNum.HasValue}");
+            Console.WriteLine($"dNum : {dNum.GetType()}");
+
+            num = null;
+            Console.WriteLine($"num : {num.HasValue}");
+
+            if (num != null)
+                Console.WriteLine($"num : {num.GetType()}");
+
+            // 변환
+            {
+                int? nullNum = 100;
+                int numCast = (int)nullNum;
+
+                Console.WriteLine($"nullNum : {numCast}"); 
+            }
+        }
+    }
+}
