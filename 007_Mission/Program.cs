2cb7cd3f3dfdc2e480e98ba8087bff742d4e68a0
Halusalm96 <dmadkr6004@naver.com>
2024-11-28 11:52:45 +0900

    11-28-compare

--- 007_Mission/Program.cs
+++ 007_Mission/Program.cs
@@ -10,9 +10,22 @@ namespace _007_Mission
             total = num1-- * 2 + ++num2 - 10 + --num1 * (num2++ % 2);
             // 10 * 2 + 21 - 10 + 8 * (21 % 2)
 
+            Console.WriteLine($"결과값 : {total}");
+
             num1 = 10;
             num2 = 20;
             Console.WriteLine($"계산 과정 : {num1--} * 2 + {++num2} - 10 + {--num1 } * ({num2++} % 2)");
+
+            Console.WriteLine($"다른 방법");
+            num1 = 10;
+            num2 = 20;
+            total = 0;
+            total = num1--;
+            total *= 2;
+            total += ++num2;
+            total -= 10;
+            total += --num1;
+            total *= (num2++ % 2);
 
             Console.WriteLine($"결과값 : {total}");
         }
---
+++ 016_Compare/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _016_Compare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 비교 연산자
+            // <, >
+            {
+                bool isResult1 = 5 < 3; //false
+                bool isResult2 = 5 > 3; //true
+
+                Console.WriteLine($"isResult1: " + isResult1);
+                Console.WriteLine($"isResult2: " + isResult2);
+
+                bool isResult3 = 0.14f < 0.2345f;
+                bool isResult4 = 0.14f > 0.2345f;
+
+                Console.WriteLine($"isResult3: " + isResult3);
+                Console.WriteLine($"isResult4: " + isResult4);
+            }
+
+            // ==, !=
+            {
+                bool isEqual1 = (5 == 5); //true
+                bool isEqual2 = (5 == 6); //false
+                Console.WriteLine($"(5 == 5): {isEqual1}");
+                Console.WriteLine($"(5 == 6): {isEqual2}");
+
+                bool isEqual3 = (50 != 50);
+                bool isEqual4 = (50 != 60);
+                Console.WriteLine($"(50 != 50): {isEqual3}");
+                Console.WriteLine($"(50 != 60): {isEqual4}");
+            }
+
+            // <=, >=
+            {
+                bool isResult1 = (5 >= 6); //false
+                bool isResult2 = (6 >= 6); //true
+                bool isResult3 = (10 >= 6); //true
+                bool isResult4 = (6 <= 5); //false
+
+                Console.WriteLine($"(5 >= 6): {isResult1}");
+                Console.WriteLine($"(6 >= 6): {isResult2}");
+                Console.WriteLine($"(10 >= 6): {isResult3}");
+                Console.WriteLine($"(6 <= 5): {isResult4}");
+            }
+        }
+    }
+}
